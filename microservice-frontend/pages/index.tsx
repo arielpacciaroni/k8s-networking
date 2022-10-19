365868a0728464dc99f3b025eda0d428492dc8a0
arielpacciaroni <ariel.pacciaroni@fundingcircle.com>
2022-10-19 14:53:12 -0300

    Add cluster EKS, add antiaffinity

--- microservice-frontend/pages/index.tsx
+++ microservice-frontend/pages/index.tsx
@@ -1,10 +1,36 @@
-import { useEffect } from "react"
+import { useQuery } from "@tanstack/react-query";
+import { useMemo } from "react";
+import axios from 'axios';
+import clsx from 'clsx';
 
 export default function Index() {
+	const { isLoading, error, data } = useQuery(['healthCheck'], async() => {
+		const { data } = await axios.get('http://localhost:3003/healthcheck');
 
-	useEffect(() => {
-		fetch("http://localhost:3003/healthcheck").then((res) => res.json()).then((data) => console.log(data)).catch(console.error)
-	});
+		return { data }
+	}, {
+		initialData: () => ({})
+	})
 
-	return <p>Hello world!</p>
+	const pointConnectionClassname = useMemo(() => {
+		if(isLoading) return 'bg-yellow-500';
+		if(error) return 'bg-red-500';
+
+		return 'bg-green-500';
+
+	}, [isLoading, error]);
+
+	return (
+		<main className="container mx-auto py-8">
+			<h1 className="my-4">Hello world!</h1>
+			<div className="flex items-center">
+				<div className={clsx('w-4', 'h-4', 'rounded-full', pointConnectionClassname)} />
+				<p className="ml-4 text-xs text-gray-500">
+					{isLoading && <p>Loading...</p>}
+					{error && <p>Some error!</p>}
+					{!isLoading && !error && <p>{JSON.stringify(data)}</p>}
+				</p>
+			</div>
+		</main>
+	)
 }
